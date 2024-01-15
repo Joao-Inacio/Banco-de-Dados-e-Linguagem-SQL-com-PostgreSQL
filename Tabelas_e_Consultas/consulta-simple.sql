fa87e85fe5f613c5198d3d6fda38b19174d5ad4c
Joao-Inacio <joaoinacio206@protonmail.com>
2024-01-15 11:07:30 -0300

    Consulta simples pt2

--- Tabelas_e_Consultas/consulta-simple.sql
+++ Tabelas_e_Consultas/consulta-simple.sql
@@ -9,4 +9,16 @@ SELECT 'CPF: ' || cpf || ' RG: ' ||  rg FROM cliente;
 
 SELECT * FROM cliente LIMIT 3;
 
+SELECT nome, data_nascimento FROM cliente WHERE data_nascimento > '2000-01-01';
 
+SELECT nome FROM cliente WHERE nome LIKE 'C%';
+
+SELECT nome FROM cliente WHERE nome LIKE '%c%';
+
+SELECT nome, data_nascimento FROM cliente WHERE data_nascimento BETWEEN '1990-01-01' AND '1998-01-01';
+
+SELECT nome, rg FROM cliente WHERE rg IS NULL;
+
+SELECT nome FROM cliente ORDER BY nome;
+
+SELECT nome FROM cliente ORDER BY nome DESC;
