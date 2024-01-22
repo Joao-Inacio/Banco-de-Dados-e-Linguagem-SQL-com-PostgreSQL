8e5c2f4746c87d537408e4f3bb4be13c6fb6cd0b
Joao-Inacio <joaoinacio206@protonmail.com>
2024-01-22 15:02:48 -0300

    Chaves estrangeiras Pt 2

--- Tabelas_e_Consultas/cheves-estrangeiras.sql
+++ Tabelas_e_Consultas/cheves-estrangeiras.sql
@@ -20,3 +20,39 @@ UPDATE cliente SET idprofissao = 1 WHERE idcliente IN (6, 7, 8, 13);
 
 SELECT * FROM cliente;
 
+ALTER TABLE cliente DROP nacionalidade;
+
+ALTER TABLE cliente ADD idnacionalidade INTEGER;
+
+ALTER TABLE cliente
+ADD CONSTRAINT fk_cln_idnacionalidade FOREIGN KEY (idnacionalidade) REFERENCES nacionalidade (idnacionalidade);
+
+UPDATE cliente SET idnacionalidade = 1 WHERE idcliente IN (1, 2, 3, 4, 6, 10, 11, 14);
+UPDATE cliente SET idnacionalidade = 2 WHERE idcliente IN (5, 7);
+UPDATE cliente SET idnacionalidade = 3 WHERE idcliente = 8;
+UPDATE cliente SET idnacionalidade = 4 WHERE idcliente IN (9, 13);
+
+ALTER TABLE cliente DROP complemento;
+
+ALTER TABLE cliente ADD idcomplemento INTEGER;
+
+ALTER TABLE cliente
+ADD CONSTRAINT fk_cln_idcomplemento FOREIGN KEY (idcomplemento) REFERENCES complemento (idcomplemento);
+
+UPDATE cliente SET idcomplemento = 1 WHERE idcliente IN (1, 4, 9, 13);
+
+UPDATE cliente SET idcomplemento = 2 WHERE idcliente IN (2, 3, 7);
+
+SELECT * FROM cliente;
+
+ALTER TABLE cliente DROP bairro;
+
+ALTER TABLE cliente ADD idbairro INTEGER;
+
+ALTER TABLE cliente
+ADD CONSTRAINT fk_cln_idbairro FOREIGN KEY (idbairro) REFERENCES bairro (idbairro);
+
+UPDATE cliente SET idbairro = 1 WHERE idcliente IN (1, 12, 13);
+UPDATE cliente SET idbairro = 2 WHERE idcliente IN (2, 3, 6, 8, 9);
+UPDATE cliente SET idbairro = 3 WHERE idcliente IN (4, 5);
+UPDATE cliente SET idbairro = 4 WHERE idcliente = 7;
