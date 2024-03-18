3089a9833cd456fa57c3878e5d43e2ff344113f3
Joao-Inacio <joaoinacio206@protonmail.com>
2024-03-18 10:31:46 -0300

    Inserção de dados na tabela clientes

--- Tabelas_e_Consultas/0.03-insercao-dados.sql
+++ Tabelas_e_Consultas/0.03-insercao-dados.sql
@@ -1,4 +1,4 @@
--- Active: 1705323844254@@127.0.0.1@5432@pedido
+-- Active: 1710766883456@@127.0.0.1@5432@pedido
 INSERT INTO cliente (
     idcliente, nome, cpf, rg, data_nascimento, genero, 
     profissao, nacionalidade, logradouro, numero,
@@ -6,8 +6,8 @@ INSERT INTO cliente (
 )
 VALUES (
     1, 'Manoel', '88828383821', '32323', '2001-01-30', 'M',
-    'Estudante', 'Brasileiro', 'Rua dos Bobos', '0',
-    'Casa', 'Bairro dos Bobos', 'Municipio dos Bobos', 'UF dos Bobos'
+    'Estudante', 'Brasileiro', 'Rua Joaquim Nabuco', '23',
+    'Casa', 'Cidade Nova', 'Porto União', 'SC'
 );
 
 INSERT INTO cliente (
@@ -18,5 +18,5 @@ INSERT INTO cliente (
 VALUES (
     2, 'Geraldo', '99887766554', '56565', '1987-01-04', 'M',
     'Engenheiro', 'Brasileiro', 'Rua das Lima', '200',
-    'Ap', 'Bairro centro', 'Municipio do Acre', 'AC'
+    'Ap', 'Centro', 'Poro Uniao', 'SC'
 );
