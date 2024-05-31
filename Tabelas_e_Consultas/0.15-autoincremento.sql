0a9a4c5d95a14a32ef06f3dd20eaacf8c5130114
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-31 09:48:09 -0300

    Autoincremento

--- Tabelas_e_Consultas/0.15-autoincremento.sql
+++ Tabelas_e_Consultas/0.15-autoincremento.sql
@@ -21,10 +21,14 @@ SELECT * FROM exemplo;
 -- Atualizando a tabela bairro
 SELECT * FROM bairro;
 
+-- Encontrar o próximo valor de idbairro
 SELECT MAX(idbairro) + 1 FROM bairro;
 
+-- Criar uma sequência
 CREATE SEQUENCE bairro_id_seq MINVALUE 5;
 
+-- Alterar a tabela para usar a sequência
 ALTER TABLE bairro ALTER idbairro SET DEFAULT NEXTVAL('bairro_id_seq');
 
+-- Associar a sequência à coluna
 ALTER SEQUENCE bairro_id_seq OWNED BY bairro.idbairro;
