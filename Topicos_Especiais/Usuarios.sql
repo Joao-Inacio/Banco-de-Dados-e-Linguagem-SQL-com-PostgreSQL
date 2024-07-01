f90d89ae236e1596d6f4fff064d743068b8d644b
Joao-Inacio <joaoinacio206@protonmail.com>
2024-07-01 09:30:26 -0300

    Testando o usuário Maria

---
+++ Topicos_Especiais/Usuarios.sql
@@ -0,0 +1,9 @@
+-- Testando o Usuario Maria
+
+select * from  cliente;
+
+-- Adicionado um novo dado
+insert into cliente (nome) values ('Ana');
+
+-- Tentado apaga um dado
+delete from cliente where idcliente = 18;
